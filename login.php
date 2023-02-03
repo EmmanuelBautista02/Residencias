7270d5159bad403b734bc146910148bed5c8bec2
EmmanuelBautista02 <l18020750@veracruz.tecnm.mx>
2023-02-02 20:07:24 -0600

    actualización para BD

--- login.php
+++ login.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>holi
+<?php
+try{
+    $hostname="localhost";
+	$username="root";
+	$password="root";
+    $database="sistema";
     
-</body>
-</html>
+    /*
+    $hostname="localhost";
+    $username='root';
+    $password='';
+    $database='proyecto';
+    */
+    $cn = new mysqli($hostname,$username,$password,$database);
+    //mysqli_connect($hostname,$username, $password);
+    echo "CONEXION CORRECTA";
+}catch(Exception $e){
+    echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+    die("\n Contacta al Departamento de TI");
+}
+//Prox semana son avances para que vea, de ahí la siguiente semana sería las revisiones
+//revisiones en teams.
+//algo simple con que se utilice validacion, seguridad, login, registro y movimientos a la BD, más todo lo de HTML Y CSS.
+?>
