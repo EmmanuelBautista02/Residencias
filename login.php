4482f7aa8eb3fcf55ba411abd1960ff940211d64
EmmanuelBautista02 <l18020750@veracruz.tecnm.mx>
2023-02-02 20:08:43 -0600

    PRUEBA DE BD LOCAL

--- login.php
+++ login.php
@@ -4,21 +4,10 @@ try{
 	$username="root";
 	$password="root";
     $database="sistema";
-    
-    /*
-    $hostname="localhost";
-    $username='root';
-    $password='';
-    $database='proyecto';
-    */
     $cn = new mysqli($hostname,$username,$password,$database);
-    //mysqli_connect($hostname,$username, $password);
     echo "CONEXION CORRECTA";
 }catch(Exception $e){
     echo 'Excepción capturada: ',  $e->getMessage(), "\n";
     die("\n Contacta al Departamento de TI");
 }
-//Prox semana son avances para que vea, de ahí la siguiente semana sería las revisiones
-//revisiones en teams.
-//algo simple con que se utilice validacion, seguridad, login, registro y movimientos a la BD, más todo lo de HTML Y CSS.
 ?>
