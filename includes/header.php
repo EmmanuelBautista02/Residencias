68f79c28bd3c6c9e69d84de72edee69836f99b31
EmmanuelBautista02 <l18020750@veracruz.tecnm.mx>
2023-02-06 12:36:49 -0600

    cambios para el header

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<header>
+    <img src="img/logo-bestel.png" alt="Logo de bestel" height="60px" width="110px">
+    <a href="">Contactanos</a>
+    <a href="index.php">Principal</a>
+    <a href="">Acceder</a>
+</header>
--- index.php
+++ index.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROYECTITO</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+<?php
+    include_once 'includes/header.php';
+?>
     <h1>PRUEBA DE CONEXION A BD LOCAL</h1>
     <form action="conexion.php" method="post">
         <button type="submit">ENVIAR DATOS</button>
