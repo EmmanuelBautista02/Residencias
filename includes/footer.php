d629ff4e1fd41144d9641f1e3859b7936922b4a5
NisiJared7 <nisi.jared.123@gmail.com>
2023-02-10 12:59:58 -0600

    TODO DE NUEVO

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<footer class="col mt-1">
+    <h6 class="text-center py-2">
+        &copy; 2023 Derechos reservados
+    </h6>
+</footer>
+
+<style>
+footer{
+    display: flex;
+    justify-content:space-around;
+    width: 100%;
+    background-color: #825AA1;
+    color: white;
+    width: 100%;
+    font-family: 'Montserrat';
+    
+}
+</style>
